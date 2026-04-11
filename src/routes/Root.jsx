441fe22ea6d9d95309c5649b9b6d70294ab5eab0
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2026-04-11 22:18:19 +0300

    feat: add Root layout with Navbar and Outlet

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,3 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 export default function Root() {
-  return <div>Root</div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="max-w-7xl mx-auto">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
