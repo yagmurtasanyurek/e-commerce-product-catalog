cd2fd81c7e5c0f25946a043886e768499f0cc8cf
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2026-04-12 18:32:45 +0300

    feat: add ProductCard component with link to product detail

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,26 @@
-export default function ProductCard() {
-  return <div>ProductCard</div>;
+import { Link } from "react-router-dom";
+export default function ProductCard({ product }) {
+  return (
+    <Link to={`products/:${product.id}`}>
+      <div>
+        <img
+          src={product.thumbnail}
+          alt={product.title}
+          className="aspect-square object-cover"
+        />
+      </div>
+      <div className="flex flex-col">
+        <div>
+          <p>{product.brand}</p>
+          <p>{product.title}</p>
+        </div>
+        <div>
+          <h5>★{product.rating}</h5>
+        </div>
+        <div>
+          <h5>${product.price}</h5>
+        </div>
+      </div>
+    </Link>
+  );
 }
