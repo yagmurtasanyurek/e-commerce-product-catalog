f61968b3e755293ed5ad793d84ad98aced671359
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2026-04-12 19:03:06 +0300

    feat: add Products page with useProducts hook and ProductGrid

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,3 +1,16 @@
+import { useProducts } from "../hooks/useProducts";
+import ProductGrid from "../components/ProductGrid";
+
 export default function Products() {
-  return <div>Products</div>;
+  const { data, isLoading, isError } = useProducts();
+
+  // early checks to make sure data is available, then render data
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>There's an error</p>;
+
+  return (
+    <div>
+      <ProductGrid products={data.products} />
+    </div>
+  );
 }
