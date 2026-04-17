c569bd95fc0f1e88f9b6849e42c902f36905b42f
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2026-04-17 21:32:21 +0300

    feat: add Cart page

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,3 +1,46 @@
+import { useCartStore } from "../store/cartStore";
+import { Link } from "react-router-dom";
+import CartItem from "../components/CartItem";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  const items = useCartStore((state) => state.items);
+  const clearCart = useCartStore((s) => s.clearCart);
+  const totalItem = useCartStore((s) => s.totalItem);
+  const totalPrice = useCartStore((s) => s.totalPrice);
+
+  if (items.length === 0) {
+    return (
+      <div>
+        <Link to="products">←</Link>
+        <p>Your cart is empty</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Your cart</h1>
+      <div>
+        {items.map((item) => (
+          <CartItem product={item} key={item.id} />
+        ))}
+
+        <button onClick={clearCart}>Clear Cart</button>
+      </div>
+
+      <div>
+        <h2>Order Summary</h2>
+        <div>
+          <p>Total items </p>
+          <p>{totalItem}</p>
+        </div>
+        <div>
+          <p>Total price </p>
+          <p>{totalPrice}</p>
+        </div>
+        <button onClick={clearCart}>Clear Cart</button>
+        <Link to="products"> Continue Shopping</Link>
+      </div>
+    </div>
+  );
 }
